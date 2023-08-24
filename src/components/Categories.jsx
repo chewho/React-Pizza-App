465393e78fb7327e5133cc2cceea80fcd90f308e
chewho <molocrips@gmail.com>
2023-08-24 18:39:00 +0200

    feat: add toggle on sort component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,25 +3,26 @@ import { React, useState } from "react";
 const Categories = ({ items }) => {
   const [activeItem, setActiveItem] = useState(null);
 
-  const onSelectHandler = (index) => {
+  const onSelectItem = (index) => {
     setActiveItem(index);
   };
 
   return (
     <div className="categories">
       <ul>
-        <li className={activeItem === null ? "active" : ""} onClick={() => onSelectHandler(null)}>
+        <li className={activeItem === null ? "active" : ""} onClick={() => onSelectItem(null)}>
           Все
         </li>
-        {items.map((name, index) => (
-          <li
-            className={activeItem === index ? "active" : ""}
-            onClick={() => onSelectHandler(index)}
-            key={`${name}_${index}`}
-          >
-            {name}
-          </li>
-        ))}
+        {items &&
+          items.map((name, index) => (
+            <li
+              className={activeItem === index ? "active" : ""}
+              onClick={() => onSelectItem(index)}
+              key={`${name}_${index}`}
+            >
+              {name}
+            </li>
+          ))}
       </ul>
     </div>
   );
