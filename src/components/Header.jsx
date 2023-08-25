9257e602f4824c5fa16c58657e5881a8f7f692d7
chewho <molocrips@gmail.com>
2023-08-25 16:47:24 +0200

    feat: add react router for Home, Cart pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import logoSvg from "../assets/img/pizza-logo.svg";
 import Button from "./Button";
@@ -7,14 +8,18 @@ const Header = () => {
   return (
     <div className="header">
       <div className="container">
-        <div className="header__logo">
-          <img width="38" src={logoSvg} alt="Pizza logo" />
-          <div>
-            <h1>React Pizza</h1>
-            <p>самая вкусная пицца во вселенной</p>
+        <Link to="/">
+          <div className="header__logo">
+            <img width="38" src={logoSvg} alt="Pizza logo" />
+            <div>
+              <h1>React Pizza</h1>
+              <p>самая вкусная пицца во вселенной</p>
+            </div>
           </div>
-        </div>
-        <Button />
+        </Link>
+        <Link to="/cart">
+          <Button />
+        </Link>
       </div>
     </div>
   );
