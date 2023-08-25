8a6c7ac93d11e0844a589bd19f510cc138293f0c
chewho <molocrips@gmail.com>
2023-08-25 20:03:01 +0200

    feat: add dummy server, use fetch for render pizzas

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import { Home, Cart } from "./pages";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3000/db.json")
+      .then((resp) => resp.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route exact path="/" Component={Home} />
+          <Route exact path="/" element={<Home items={pizzas} />} />
           <Route exact path="/cart" Component={Cart} />
         </Routes>
       </div>
