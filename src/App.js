7e01bb4f8447ae2d54f0a1293162762499ff6761
chewho <molocrips@gmail.com>
2023-08-29 19:52:44 +0200

    feat: add cart, pizzas reducers, add cart, filters, pizzas actions

--- src/App.js
+++ src/App.js
@@ -5,26 +5,30 @@ import { Header } from "./components";
 import { Home, Cart } from "./pages";
 import { Route, Routes } from "react-router-dom";
 
-function App() {
-  const [pizzas, setPizzas] = React.useState([]);
+// function App() {
+//   React.useEffect(() => {
+//     axios.get("http://localhost:3000/db.json").then(({ data }) => {
+//       setPizzas(data.pizzas);
+//     });
+//   }, []);
 
-  React.useEffect(() => {
-    axios.get("http://localhost:3000/db.json").then(({ data }) => {
-      setPizzas(data.pizzas);
-    });
-  }, []);
+//   return;
+// }
 
-  return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route exact path="/" element={<Home items={pizzas} />} />
-          <Route exact path="/cart" Component={Cart} />
-        </Routes>
+class App extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home items={[]} />} />
+            <Route exact path="/cart" Component={Cart} />
+          </Routes>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
