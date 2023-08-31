68b9a193ba260bdde0215e1392dfb21310644010
chewho <molocrips@gmail.com>
2023-08-31 19:34:23 +0200

    refactor: rewrite App on functional component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,38 @@
 import React from "react";
 import axios from "axios";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import { Header } from "./components";
 import { Home, Cart } from "./pages";
 import { Route, Routes } from "react-router-dom";
-import { setPizzas as setPizzasAction } from "./redux/actions/pizzas";
+import { setPizzas } from "./redux/actions/pizzas";
 
-// function App() {
-//   React.useEffect(() => {
-//     axios.get("http://localhost:3000/db.json").then(({ data }) => {
-//       setPizzas(data.pizzas);
-//     });
-//   }, []);
+const App = () => {
+  const dispatch = useDispatch();
+  const { items } = useSelector(({ pizzas, filters }) => {
+    return {
+      items: pizzas.items,
+      sortBy: filters.sortBy,
+    };
+  });
 
-//   return;
-// }
-
-class App extends React.Component {
-  componentDidMount() {
+  React.useEffect(() => {
     axios.get("http://localhost:3000/db.json").then(({ data }) => {
-      this.props.setPizzas(data.pizzas);
+      dispatch(setPizzas(data.pizzas));
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route exact path="/" element={<Home items={this.props.items} />} />
-            <Route exact path="/cart" Component={Cart} />
-          </Routes>
-        </div>
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Routes>
+          <Route exact path="/" element={<Home items={items} />} />
+          <Route exact path="/cart" Component={Cart} />
+        </Routes>
       </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    items: state.pizzas.items,
-  };
+    </div>
+  );
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    setPizzas: (items) => dispatch(setPizzasAction(items)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,9 +17,7 @@ const Home = ({ items }) => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {items.map((obj) => (
-          <PizzaBlock key={obj.id} {...obj} />
-        ))}
+        {items && items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
     </div>
   );
